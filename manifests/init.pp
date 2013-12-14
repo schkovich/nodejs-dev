0f83b613eea41676521bd6f07efe3546bfcea003
gmiskovic <goran.v.miskovic@gmail.com>
2013-12-14 14:56:23 +0100

    Installing tcpproxy using npm

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,16 +14,30 @@ class nodejs_dev (
   $host = $nodejs_dev::conf::host, $port = 8080, $user = 'vagrant'
 ) inherits nodejs_dev::conf {
 
-  include nodejs_dev::user
-  include nodejs_dev::nodejs_install
-  include nodejs_dev::redis_install
+  class {"nodejs_dev::user":}
+
+  package {"git":
+    ensure => "installed",
+    provider => "apt",
+    require => Class['nodejs_dev::user']
+  }
 
   file {
     "/home/${user}/opt":
-      ensure => directory,
-      group  => $user,
-      owner  => $user,
-      mode   => 0755;
+    ensure => directory,
+    group  => $user,
+    owner  => $user,
+    mode   => 0755,
+    require => Package["git"],
+  }
+
+  class {'nodejs_dev::nodejs_install':
+    user => $user,
+    subsribe => File["/home/${user}/opt"],
+  }
+
+  class {'nodejs_dev::redis_install':
+    require => Class['nodejs_dev::nodejs_install'],
   }
 }
 
--- manifests/nodejs_install.pp
+++ manifests/nodejs_install.pp
@@ -1,5 +1,10 @@
-class nodejs_dev::nodejs_install ($manage_repo = true) {
+class nodejs_dev::nodejs_install ($manage_repo = true, $user) {
   validate_bool($manage_repo)
 
   class { 'nodejs': manage_repo => $manage_repo }
+
+  nodejs::npm { "/home/${user}/opt:git+https://github.com/schkovich/tcpproxy.git":
+    ensure  => present,
+    require => Class['nodejs'],
+  }
 }
