5bbd87a2c83fd75908b99fe3bf0c4f42facd15db
Goran Miskovic <schkovich@gmail.com>
2015-01-03 08:31:46 +0100

    Added required Mongo client. Removed auth parameter.

--- manifests/mongodb_install.pp
+++ manifests/mongodb_install.pp
@@ -7,22 +7,22 @@ class nodejs_dev::mongodb_install ($manage_repo = true) {
   }
   ->
   class {'::mongodb::server':
-    port    => 27018,
+    port    => 27017,
     verbose => true,
-    auth => true,
   }
-
+  ->
+  class {'mongodb::client':}
+  ->
   mongodb_database { 'vatdb':
     ensure   => present,
     tries    => 10,
-    require  => Class['mongodb::server'],
   }
   ->
   mongodb_user { vatuser:
     username      => 'vateu',
     ensure        => present,
     password_hash => '6c59af3bd58968e1392a2210bce198a5',
-#    password_hash => mongodb_password('testuser', 'p@ssw0rd'),
+  #    password_hash => mongodb_password('testuser', 'p@ssw0rd'),
     database      => 'vatdb',
     roles         => ['readWrite', 'dbAdmin'],
     tries         => 10,
