9f38f5703e1d2e1500d279c6ca21939855d55ec3
Goran Miskovic <schkovich@gmail.com>
2015-01-03 05:34:22 +0100

    Sometimes it is good to actually define class. :(

--- manifests/mongodb_install.pp
+++ manifests/mongodb_install.pp
@@ -1,14 +1,20 @@
-class {'::mongodb::globals':
-    manage_package_repo => true,
-}
-->
-class {'::mongodb::server':
+class nodejs_dev::mongodb_install ($manage_repo = true) {
+
+  validate_bool($manage_repo)
+
+  class {'::mongodb::globals':
+    manage_package_repo => $manage_repo,
+  }
+  ->
+  class {'::mongodb::server':
     auth => true,
+  }
+  ->
+  ::mongodb::db { 'vatdb':
+    user          => 'vateu',
+    password_hash => '6c59af3bd58968e1392a2210bce198a5',
+  }
+  ->
+  class {'::mongodb::client': }
+
 }
-->
-mongodb::db { 'vatdb':
-  user          => 'vateu',
-  password_hash => '6c59af3bd58968e1392a2210bce198a5',
-}
-->
-class {'::mongodb::client': }
