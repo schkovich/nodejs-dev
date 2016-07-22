fecfe36c1b4ac23f0a6ff80760d09dbc9754b124
Goran Miskovic <schkovich@gmail.com>
2016-07-23 01:54:16 +0200

    removed install directory.

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,25 +14,12 @@ class nodejs_dev (
   $host = $nodejs_dev::conf::host,
   $port = 8080,
   $user = 'vagrant',
-  $install_dir = ''
 ) inherits nodejs_dev::conf {
 
-  case $install_dir {'': { $install_dir = "/home/${user}/opt" } }
-
-  file {
-    "${install_dir}":
-    ensure => directory,
-    group  => $user,
-    owner  => $user,
-    mode   => 0755,
-  }
-
-  class {"nodejs_dev::user":
-    subscribe => File["${install_dir}"],
-  }
+  class {"nodejs_dev::user": }
   ->
   class { 'nodejs':
-    repo_url_suffix => '5.x',
+    repo_url_suffix => '6.x',
   }
 
 }
